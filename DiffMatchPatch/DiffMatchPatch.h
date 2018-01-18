e23b62ac5cab467f19636d75ce185a2c71cf1a9a
Bart Whiteley <bwhiteley@gmail.com>
2018-01-17 20:17:51 -0700

    Fixes for Carthage

---
+++ DiffMatchPatch/DiffMatchPatch.h
@@ -0,0 +1,18 @@
+//
+//  DiffMatchPatch.h
+//  DiffMatchPatch
+//
+//  Created by Bart Whiteley on 1/17/18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DiffMatchPatch.
+FOUNDATION_EXPORT double DiffMatchPatchVersionNumber;
+
+//! Project version string for DiffMatchPatch.
+FOUNDATION_EXPORT const unsigned char DiffMatchPatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DiffMatchPatch/PublicHeader.h>
+
+
